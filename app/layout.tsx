ed09068d87b620eb0bc0642205de6f82ac4536df
tein <92306279+kerenIy@users.noreply.github.com>
2025-05-28 10:26:41 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,22 +17,21 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <head>
-      <Script
-        src="https://chatbot.ankursolutions.com/widget.js"
-        strategy="afterInteractive"
-        data-name="DNILBot"
-        data-token="gAAAAABoNXl6Ptb78SFMAdRohLEgRFHVAINWHWhfLsT1Qwbe7xyzC4YoidSS1uBiIROEGR5c3-wxcz3gPXFj64UtAEmui-EoM2RmftWJ3N4O1QS_2BXFB4c=--gAAAAABoNXl6_e1dbd5gSc9uiiX58wHjOwcdPCllE_rnGXTjF8qc2YoJtGLhSqHSs0XeIW7-16txbMPjeYcFcriE00g4RYH9-Q==--gAAAAABoNXl6eG8S7_V1oXnHD4B_aJFdhXwKgZzlWLhf9vSgJMCTaZMBXzrD7HkQ6dJ-zA0N-_cXxK5X1dr67LDWprtQz5hfZPyrrHf0AUuONsA0vZOnkC8="
-        async
-      />
-      </head>
+    <html lang="en"
       <body className={`${noto_sans.className} overflow-x-hidden bg-white`}>
-        <AOSProvider>
-          {children}
-          <Footer />
-        </AOSProvider>
-      </body>
+  <Script
+    src="https://chatbot.ankursolutions.com/widget.js"
+    strategy="afterInteractive"
+    data-name="DNILBot"
+    data-token="gAAAAABoNXl6Ptb78SFMAdRohLEgRFHVAINWHWhfLsT1Qwbe7xyzC4YoidSS1uBiIROEGR5c3-wxcz3gPXFj64UtAEmui-EoM2RmftWJ3N4O1QS_2BXFB4c=--gAAAAABoNXl6_e1dbd5gSc9uiiX58wHjOwcdPCllE_rnGXTjF8qc2YoJtGLhSqHSs0XeIW7-16txbMPjeYcFcriE00g4RYH9-Q==--gAAAAABoNXl6eG8S7_V1oXnHD4B_aJFdhXwKgZzlWLhf9vSgJMCTaZMBXzrD7HkQ6dJ-zA0N-_cXxK5X1dr67LDWprtQz5hfZPyrrHf0AUuONsA0vZOnkC8="
+    async
+  />
+  <AOSProvider>
+    {children}
+    <Footer />
+  </AOSProvider>
+</body>
+
     </html>
   );
 }
