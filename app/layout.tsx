f693e1108e23837b89da4d1c8b954096eb0d5572
tein <kerp4real@gmail.com>
2025-05-28 11:03:39 +0100

    fix: chatbot deployment

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,6 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <script
-          async
           src="https://chatbot.ankursolutions.com/widget.js"
           data-name="DNILBot"
           data-token="gAAAAABoNXl6Ptb78SFMAdRohLEgRFHVAINWHWhfLsT1Qwbe7xyzC4YoidSS1uBiIROEGR5c3-wxcz3gPXFj64UtAEmui-EoM2RmftWJ3N4O1QS_2BXFB4c=--gAAAAABoNXl6_e1dbd5gSc9uiiX58wHjOwcdPCllE_rnGXTjF8qc2YoJtGLhSqHSs0XeIW7-16txbMPjeYcFcriE00g4RYH9-Q==--gAAAAABoNXl6eG8S7_V1oXnHD4B_aJFdhXwKgZzlWLhf9vSgJMCTaZMBXzrD7HkQ6dJ-zA0N-_cXxK5X1dr67LDWprtQz5hfZPyrrHf0AUuONsA0vZOnkC8="
